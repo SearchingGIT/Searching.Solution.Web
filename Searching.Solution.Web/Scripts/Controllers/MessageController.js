26c2e500ba0b347dba9d7e6d41cc6378c43f823b
Alex <fursaalex@gmail.com>
2016-10-19 05:12:06 +0300

    reworking mainpage, registration, login/clean

--- Searching.Solution.Web/Scripts/Controllers/MessageController.js
+++ Searching.Solution.Web/Scripts/Controllers/MessageController.js
@@ -1,13 +1,5 @@
 ﻿
 var MessageController = function (ApiService) {
-    var ann = new Ann();
-    ann.Categories_id = 1;
-    ann.Info_Announcing = 'test';
-    ann.Name_Announcing = 'test';
-    ann.City_id = 1;
-    ApiService.AddAnnouncing(ann).success(function (response) {
-        console.log(response);
-    });
     console.clear();
     console.log('MessageControler said hello');
     
